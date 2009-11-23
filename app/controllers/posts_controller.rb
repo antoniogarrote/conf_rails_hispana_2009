774c9e03fe01b5334b201d08aabd54179368920a
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2009-11-23 23:44:58 +0100

    OMGOMG security flaw! and some eyecandy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
 
-  before_filter :authenticate, :only => [:create, :new]
+  before_filter :authenticate, :only => [:create, :new, :update, :edit]
+  before_filter :verify_blog_and_owner, :only => [:new, :edit]
 
   def show
-    begin
       @blog = Blog.find(:by_id, params[:blog_id])
       @post = Post.find(:by_id, params[:id])
       @comments = @post.relation(:comments)
@@ -11,14 +11,9 @@ class PostsController < ApplicationController
     rescue Exception => ex
       flash[:error] = "Impossible to find blog: #{ex.message}"
       redirect_to :back
-    end
   end
 
   def new
-    if params[:blog_id].nil?
-      flash[:error] = "A blog identifier is required to create a new post"
-      redirect_to :back
-    end
     @blog = Blog.find(:by_id, params[:blog_id])
   end
 
@@ -86,6 +81,21 @@ class PostsController < ApplicationController
 
     rescue Exception => ex
       flash[:error] = "error destroying post data: #{ex.message}"
+      redirect_to :back
+    end
+  end
+
+  private
+ 
+  def verify_blog_and_owner
+    if params[:blog_id]
+      @blog = Blog.find :by_id, params[:blog_id]
+      if @blog.get(:user_id) != current_user.get(:id)
+        flash[:error] = "Hey you! This blog is not yours! You can't create a post."
+        redirect_to :back
+      end
+    else
+      flash[:error] = "A blog identifier is required to create a new post"
       redirect_to :back
     end
   end
